eb4e29666b88d59df11ed78b83c15c4b9fa0cfec
AranyaAryaman <aryamanaranya@gmail.com>
2020-04-04 00:32:59 +0530

    Optional Assignments added

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -51,6 +51,7 @@ fprintf('Normalizing Features ...\n');
 
 [X mu sigma] = featureNormalize(X);
 
+
 % Add intercept term to X
 X = [ones(m, 1) X];
 
@@ -82,16 +83,24 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha1 = 0.01;
+%alpha2 = 1.29;
+%alpha3 = 0.03;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
-theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+theta1=theta2=theta = zeros(3, 1);
+[theta, J_history] = gradientDescentMulti(X, y, theta, alpha1, num_iters);
+%[theta1, J_history1] = gradientDescentMulti(X, y, theta1, alpha2, num_iters);
+%[theta2, J_history2] = gradientDescentMulti(X, y, theta2, alpha3, num_iters);
 
 % Plot the convergence graph
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+%hold on;
+%plot(1:numel(J_history1), J_history1, '-r', 'LineWidth', 2);
+%plot(1:numel(J_history2), J_history2, '-k', 'LineWidth', 2);
+
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,7 +113,10 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+
+a = (1650 - mu(1))/sigma(1);
+b = (3 - mu(2))/sigma(2);
+price = theta(1) + theta(2) * a + theta(3) * b; % You should change this
 
 
 % ============================================================
@@ -149,7 +161,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = theta(1) + theta(2)*1650 +theta(3)*3;; % You should change this
 
 
 % ============================================================
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,16 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
-
+    error = (X * theta) - y;
+    theta = theta - ((alpha/m) * X'*error);
+    
     % ============================================================
 
     % Save the cost J in every iteration    
